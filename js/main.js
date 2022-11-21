2534f1247983c53624e82865ec5ce525e435c841
Dennis Empirio <dennis.empirio@gmail.com>
2022-11-21 15:17:05 +0100

    img saved, arrCard added

---
+++ js/main.js
@@ -0,0 +1,50 @@
+// CREARE UN ARRAY DI OGGETTI PER RAPPRESENTARE I MEMBRI DEL TEAM
+
+// ogni membro è caratterizzato da: nome, ruolo e foto
+
+// ---------------------------------------------------------------
+
+// 1. Creare array di oggetti con le informazioni necessaree
+
+// 2. Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+// 3. Stampare le stesse informazione sul DOM sottoforma di stringhe
+
+let ArrCard = [
+    {
+        "Nome" : "Wayne Barnett",
+        "Ruolo" : "Founder & CEO",
+        "foto" : "img"
+    },
+
+    {
+        "Nome" : "Angela Caroll",
+        "Ruolo" : "Chief Editor",
+        "foto" : "img"
+    },
+
+    {
+        "Nome" : "Walter Gordon",
+        "Ruolo" : "Office Manager",
+        "foto" : "img"
+    },
+
+    {
+        "Nome" : "Angela Lopez",
+        "Ruolo" : "Social Media Manager",
+        "foto" : "img"
+    },
+
+    {
+        "Nome" : "Scott Estrada",
+        "Ruolo" : "Developer",
+        "foto" : "img"
+    },
+
+    {
+        "Nome" : "Barbara Ramos",
+        "Ruolo" : "Graphic Designer",
+        "foto" : "img"
+    },
+    
+]
